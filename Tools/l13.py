276755c1b1f50f09df459a3692c45d47c885502e
DiAlpatov <geas161rus@gmail.com>
2026-02-09 14:14:18 +0300

    тулзы

---
+++ Tools/l13.py
@@ -0,0 +1,10 @@
+# data_file = open('data.txt', 'r')
+# data_file.read()
+# data_file.close()
+
+with open('FullQA/Tools/data.txt', 'r') as data_file:
+  data = data_file.read()
+
+print(data)
+
+# продолжить позже
